699a95c2c4bcf08e14f42c4263283e7c3b50eb01
gulyolik <olia.guliaeva@yandex.ru>
2022-12-21 23:26:51 +0300

    Commit in process

---
+++ src/main/java/ru/netology/FilmslistManager.java
@@ -0,0 +1,45 @@
+package ru.netology;
+
+import ru.netology.domain.Filmslist;
+import ru.netology.repository.FilmslistRepository;
+public class FilmslistManager {
+    private FilmslistRepository repo;
+    int maxNumber;
+
+    public FilmslistManager(FilmslistRepository repo) {
+        this.repo = repo;
+        this.maxNumber = 10;
+
+    }
+
+    public FilmslistManager(FilmslistRepository repo, int maxNumber){
+
+        this.maxNumber = maxNumber;
+    }
+
+    public void add (Filmslist list){
+
+        repo.save(list);
+    }
+    public Filmslist[] findALl() {
+
+        return repo.getFilms();
+    }
+
+    public Filmslist[] findLast() {
+        Filmslist[] all = findALl();
+        int resultLength;
+        if (all.length <= maxNumber) {
+            resultLength = all.length;
+        } else {
+            resultLength = maxNumber;
+        }
+        Filmslist[] reversed = new Filmslist[resultLength];
+        for (int i = 0; i < reversed.length; i++) {
+            reversed[i] = all[all.length - 1 - i];
+        }
+        return reversed;
+    }
+
+}
+
